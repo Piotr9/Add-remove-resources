cf00d3e7c17a380991b720c165982d7ab9e7147e
Piotr9 <33264584+Piotr9@users.noreply.github.com>
2021-01-04 01:27:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <dir>
+        <the-header title="RememberMe"></the-header>
+        <the-resources></the-resources>
+    </dir>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+    export default {
+        components: {
+            TheHeader,
+            TheResources
+        },
+    }
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+</style>
---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button :class="mode"><slot></slot></button>
+</template>
+
+<script>
+    export default {
+        props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+    button{
+        color: #fff;
+        text-transform: uppercase;
+        text-decoration: none;
+        background: #60a3bc;
+        padding: 20px;
+        border-radius: 12px;
+        display: inline-block;
+        border: none;
+        transition: all 0.4s ease 0s;
+        cursor: pointer;
+    }
+    button:hover {
+        text-shadow: 0px 0px 5px rgba(255, 255, 255, 1);
+        box-shadow: orchid;
+        -webkit-box-shadow: 0px 5px 20px -10px rgba(0,0,0,0.57);
+        -moz-box-shadow: 0px 5px 20px -10px rgba(0,0,0,0.57);
+        transition: all 0.4s ease 0s;
+    }
+    .flat {
+        background-color: transparent;
+        color: #1b0004;
+        border: none;
+    }
+    .flat:hover,
+    .flat:active {
+        background-color: #640032;
+        color: white;
+    }
+
+</style>
